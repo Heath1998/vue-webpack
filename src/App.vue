7d577e20d57fba562e0daf3ae4e67c566213c666
“zehongfan <fanzehongemail@163.com>
2020-11-02 20:11:43 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="App">
+    <div>
+      <i class="one-icon-camera"></i>
+      <img src="./assets/img/logo.png" alt="">
+      <router-link to="/Home">go Home</router-link>
+      <router-link to="/About">go About</router-link>
+    </div>
+    <div>
+      <p>{{getCounter}}</p>
+      <button @click="add">add</button>
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  name: 'App',
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(['getCounter'])
+  },
+
+  methods: {
+    ...mapActions(['add'])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import './assets/styles/var.scss';
+.App {
+  text-align: center;
+  color: $primary-color;
+  font-size: 28px;
+}
+</style>
